fab7b09db43b6f6ccf53ca335f20bed07ce6ed84
Tejas Gawande <tejugawande25@gmail.com>
2024-02-11 17:08:10 +0530

    changes

--- wheather-frontend/src/components/dashboard/dashboard.js
+++ wheather-frontend/src/components/dashboard/dashboard.js
@@ -1,17 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./dashboard.css";
-import search from "./images/search.png";
-import rain from "./images/rain.png";
-import humidity from "./images/humidity.png";
-import wind from "./images/wind.png";
+import searchImage from "./images/search.png";
+import humidityImage from "./images/humidity.png";
+import windImage from "./images/wind.png";
+import cloudImage from "./images/cloudImage.png";
 
 function Dashboard() {
-    const[input, setInput] = useState("");
+  const [city, setCity] = useState("delhi");
+  const [input, setInput] = useState("");
+  const [humidity, setHumidity] = useState("");
+  const [speed, setSpeed] = useState("");
+  const [temp, setTemp] = useState("");
+  console.log(city);
 
-    const handleInput = (e) =>{
-        setInput(e.target.value)
-    }
-    console.log(input);
+  let url = `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`;
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "1d2890e8b5mshd15f4223fef71fap1e159cjsncbeb7aec12f1",
+      "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+    },
+  };
+
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    setCity(input);
+
+    fetch(url, options)
+      .then((response) => response.json())
+      .then((response) => {
+        setTemp(response.temp);
+        setHumidity(response.humidity);
+        setSpeed(response.wind_speed);
+      })
+      .catch(() => console.log("wait"));
+  };
+
   return (
     <>
       <div className="container">
@@ -20,33 +47,42 @@ function Dashboard() {
             <input
               type="text"
               placeholder="Enter your city name."
-            //   spellcheck="false"
+              //   spellcheck="false"
               onChange={handleInput}
             />
             <button>
-              <img src={search} alt="button-image" type="submit" />
+              <img
+                src={searchImage}
+                alt="button-image"
+                type="submit"
+                onClick={handleSubmit}
+              />
             </button>
           </div>
           <div className="whether">
-            <img src={rain} className="whether-icon" />
-            <h1 className="temp">22°</h1>
-            <h2 className="city">New York</h2>
+            <img src={cloudImage} className="whether-icon" />
+            <h1 className="temp">{temp}°</h1>
+            <h2 className="tempk">{temp * 1.8 + 32} °F</h2>
+            <h2 className="city">{input}</h2>
           </div>
           <div className="details">
             <div className="col">
-              <img src={humidity}/>
+              <img src={humidityImage} />
               <div>
-                <p className="humidity">50%</p>
+                <p className="humidity">{humidity}</p>
                 <p>Humidity</p>
               </div>
             </div>
             <div className="col">
-              <img src={wind} />
+              <img src={windImage} />
               <div>
-                <p className="wind">15 K</p>
+                <p className="wind">{speed} K</p>
                 <p>Wind Speed</p>
               </div>
             </div>
+          </div>
+          <div style={{marginTop:"15px"}}>
+            <a href="/records">Previous</a>
           </div>
         </div>
       </div>
